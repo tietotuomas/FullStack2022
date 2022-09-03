8e8da8cb797d95576b39df535b806762fbc42449
Tuomas <tietotuomas@gmail.com>
2022-09-04 00:16:12 +0300

    Add exercises up to 2.11

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,18 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import FilterField from "./components/FilterField"
 import NameForm from "./components/NameForm"
 import PersonList from "./components/PersonList"
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ])
+  const [persons, setPersons] = useState([])
+
   const [filter, SetFilter] = useState("")
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+
+  useEffect(() => {
+    const eventHandler = (response) => {
+      console.log(response.data)
+      setPersons(response.data)
+    }
+    console.log("effect")
+    axios.get("http://localhost:3001/persons").then(eventHandler)
+  }, [])
 
   let filteredPersons = persons
   if (filter) {
