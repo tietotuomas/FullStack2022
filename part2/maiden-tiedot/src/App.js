b8ec5e9037e3f1356c693099091c86461b6fe9ab
Tuomas <tietotuomas@gmail.com>
2022-09-07 01:39:12 +0300

    Add exercise 2.14

--- part2/maiden-tiedot/src/App.js
+++ part2/maiden-tiedot/src/App.js
@@ -6,51 +6,47 @@ import Countries from "./components/Countries"
 const App = () => {
   const [countries, setCountries] = useState([])
   const [countriesToShow, setCountriesToShow] = useState([])
-  // const [countryToShow, setCountryToShow] = useState([])
+  const [city, setCity] = useState("")
+  const [cityWeather, setCityWeather] = useState([])
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      // const mappedCountries = response.data.map(c=>({name: c.name.common, capital: c.capital, population: c.population, languages: c.languages}))
       setCountries(response.data)
     })
   }, [])
 
+  useEffect(() => {
+    if (city) {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+        )
+        .then((response) => {
+          setCityWeather(response.data)
+        })
+    }
+  }, [city])
+
   const handleFilterChange = (event) => {
-    setCountriesToShow(
-      countries.filter((c) =>
-        c.name.common.toLowerCase().includes(event.target.value.toLowerCase())
-      )
+    const countryList = countries.filter((c) =>
+      c.name.common.toLowerCase().includes(event.target.value.toLowerCase())
     )
-    console.log("EVENTTI", event.target.value)
-  }
-
-  // const showCountry = (event) => {
-  //   event.preventDefault()
-  //   console.log("submit");
-  //   console.log(countryToShow);
-    // setCountriesToShow(
-    //   countries.filter((c) =>
-    //     c.name.common.toLowerCase().includes(countryToShow)
-    //   )
-    // )
-  // }
-
-  const handleShowChange = (event) => {
-    setCountriesToShow(
-      countries.filter((c) =>
-        c.name.common.toLowerCase().includes(event.target.value.toLowerCase())
-      )
-    )
+    setCountriesToShow(countryList)
+    if (countryList.length === 1) {
+      setCity(countryList[0].capital)
+    }
   }
 
   return (
     <div>
-      <h1>Country database (by REST API)</h1>
+      <h1>
+        Country database (by REST<small>ful</small> API)
+      </h1>
       <Filter handleChange={handleFilterChange} />
       <Countries
         countriesToShow={countriesToShow}
-        // showCountry={showCountry}
-        handleShowChange={handleShowChange}
+        handleChange={handleFilterChange}
+        weather={cityWeather}
       />
     </div>
   )
--- part2/maiden-tiedot/src/components/Countries.js
+++ part2/maiden-tiedot/src/components/Countries.js
@@ -1,11 +1,11 @@
-const Countries = ({ countriesToShow, handleShowChange }) => {
+const Countries = ({ countriesToShow, handleChange, weather }) => {
   if (countriesToShow.length > 10) {
     return <p>Too many search results</p>
   } else if (countriesToShow.length > 1) {
     return countriesToShow.map((country) => (
       <div key={country.name.common}>
         {country.name.common}
-        <button onClick={handleShowChange} value={country.name.common}>
+        <button onClick={handleChange} value={country.name.common}>
           show
         </button>
       </div>
@@ -14,26 +14,52 @@ const Countries = ({ countriesToShow, handleShowChange }) => {
     const languages = Object.entries(countriesToShow[0].languages).map((l) => (
       <li key={l[0]}>{l[1]}</li>
     ))
-
-    console.log(countriesToShow)
-    return (
-      <div>
-        <h3>{countriesToShow[0].name.common}</h3>
-        <br></br>
-        capital: {countriesToShow[0].capital[0]}
-        <br></br>
-        population: {countriesToShow[0].population}
-        <br></br>
-        <p>languages:</p>
-        <ul>{languages}</ul>
-        <br></br>
-        <img
-          src={countriesToShow[0].flags.png}
-          alt={`flag of ${countriesToShow[0].name.common}`}
-          style={{ width: "15%", height: "15%" }}
-        />
-      </div>
-    )
+    if (weather.length !== 0) {
+      return (
+        <div>
+          <h3>{countriesToShow[0].name.common}</h3>
+          capital: {countriesToShow[0].capital[0]}
+          <br></br>
+          population: {countriesToShow[0].population}
+          <br></br>
+          <p>languages:</p>
+          <ul>{languages}</ul>
+          <br></br>
+          <img
+            src={countriesToShow[0].flags.png}
+            alt={`flag of ${countriesToShow[0].name.common}`}
+            style={{ width: "15%", height: "15%" }}
+          />
+          <h4>{`Weather in ${countriesToShow[0].capital}`}</h4>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={`An icon for weather in ${countriesToShow[0].capital}`}
+          />
+          <br></br>
+          temperature: {weather.main.temp} (feels like {weather.main.feels_like}
+          )<br></br>
+          wind: {weather.wind.speed}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h3>{countriesToShow[0].name.common}</h3>
+          capital: {countriesToShow[0].capital[0]}
+          <br></br>
+          population: {countriesToShow[0].population}
+          <br></br>
+          <p>languages:</p>
+          <ul>{languages}</ul>
+          <br></br>
+          <img
+            src={countriesToShow[0].flags.png}
+            alt={`flag of ${countriesToShow[0].name.common}`}
+            style={{ width: "15%", height: "15%" }}
+          />
+        </div>
+      )
+    }
   }
 }
 
