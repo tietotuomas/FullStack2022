0292f07019944456cd7582f843ae62ca643eae6d
Tuomas <tietotuomas@gmail.com>
2022-09-19 01:36:28 +0300

    Add exercise 3.9

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 morgan.token('person', (req, res) => {
   return JSON.stringify(req.body)
 })
@@ -74,7 +76,7 @@ const generateId = () => {
   return id
 }
 
-const PORT = 3001
+const PORT = process.enc.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
